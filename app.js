fec6f1bb406aaf967282c8a3b94e7471833c9d2b
Yogesh Kumar <yogesh19921@gmail.com>
2023-10-13 01:29:12 +0000

    Express now

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require('express');
-const httpProxy = require('http-proxy');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const proxy = httpProxy.createProxyServer();
 
-// Replace with your target host and port
-const targetHost = '24.84.245.211';
-const targetPort = 8080;
+app.use(
+  '/*',
+  createProxyMiddleware({
+    target: 'http://24.84.245.211:8080',
+    changeOrigin: true,
+  }),
+);
 
-app.use((req, res) => {
-    proxy.web(req, res, {
-        target: `${targetHost}:${targetPort}`
-    });
-});
-
-proxy.on('error', (err, req, res) => {
-    res.writeHead(500, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Something went wrong. And we are reporting a custom error message.');
-});
-
-app.listen(localport, () => {
-    console.log(`Server running on port 8000, proxying requests to ${targetHost}:${targetPort}`);
-});
+app.listen(process.env.PORT);
